1f244ac39c287e2486d248bd15f152d0bb198771
gali Ellenblum <16866059+galielle@users.noreply.github.com>
2019-01-30 14:03:10 -0500

    Update HG_all1.py

--- HG_all1.py
+++ HG_all1.py
@@ -13,14 +13,17 @@ import os
 import glob
 import HGlearn12 as hg
 
+######
+
+## The main function, calling the scripts that either run the GLA normally, remove one constraint at a time,
+## or run consecutively for all participant files in a directory
 
 def main():
-    flags = ['hg', 'minusone', 'all']
+    flags = ['hg', 'minusone', 'all', 'countcands']
     flag = get_flag(flags)
 
     while True:
         print "Default values for this script:\n" \
-              "EvalFilesDirectory = current directory\n" \
               "Iterations = 1000\n" \
               "Rate of learning = 0.1\n" \
               "Weights initialized to random values\n"
@@ -31,23 +34,23 @@ def main():
         else:
             break
     if default_vals == 'y':
-        eval_files_dir = os.getcwd()
         iterations = 1000
         rate = 0.1  # change here, suggested 0.1
         uni = 0  # 0 = random initialization of weights; 1 = initialize all weights to 1.
     else:
-        change_dir = raw_input('Change the directory of Eval files from current directory? (y/n) ').lower()
-        if change_dir == 'n':
-            eval_files_dir = os.getcwd()
-        else:
-            eval_files_dir = raw_input('Enter the directory where eval files are located: ')
-        print "Eval files directory set to %s" % (eval_files_dir)
         iterations = int(raw_input('Enter the number of iterations: '))
         rate = float(raw_input('Enter the learning rate: '))
         uni = int(raw_input('Weights initialized to random values (0) or uniform values (1): '))
 
+    change_dir = raw_input('Change the directory of Eval files from current directory? (y/n) ').lower()
+    if change_dir == 'n':
+        eval_files_dir = os.getcwd()
+    else:
+        eval_files_dir = raw_input('Enter the directory where eval files are located: ')
+    print "Eval files directory set to %s" % (eval_files_dir)
+
     if flag == 'hg':
-        hg_letters = harmonic_grammar(eval_files_dir, iterations, rate, uni, override = 1) # Override = 0 to create a new logfile for each run
+        hg_letters = harmonic_grammar(eval_files_dir, iterations, rate, uni, override = 0) # Override = 0 to create a new logfile for each run
 
     elif flag == 'minusone':
         minus1 = minusone(eval_files_dir, iterations, rate, uni)
@@ -56,17 +59,22 @@ def main():
         great_success = run_all_hg(eval_files_dir, iterations, rate, uni)
         print great_success
 
+    elif flag == 'countcands':
+        cand_nums = count_cands(eval_files_dir)
+
+
     else:
         exit()
 
-
 ################
 
+# Get the user-selected flag to decide which version to run
 def get_flag(flags):
     not_flag = 'Use with one of the following options:\n' \
                'hg - calculate the optimal weights based on given constraints file and targets file\n' \
                'minusOne - run the script removing one of the constraints each time\n' \
-               'all - run the script on all target files in a given directory\n'
+               'all - run the script on all target files in a given directory\n' \
+               'countCands - return the number of candidates for each letter-shape and for all of the letter shapes in a given targets file'
 
     if len(sys.argv) != 2:
         print not_flag
@@ -94,6 +102,7 @@ def get_flag(flags):
 
 ####################
 
+## Calls the main function from the latest version of the HGlearn script
 def harmonic_grammar(eval_files_dir, iterations, rate, uni, override):
     ### Define basic parameters and get initial input
     constraints_file = raw_input('Enter the name of the constraints file: ')
@@ -104,7 +113,7 @@ def harmonic_grammar(eval_files_dir, iterations, rate, uni, override):
     hg_letters = hg.main(eval_files_dir, constraints, targets_filename, iterations, rate, uni, override)
     return(hg_letters)
 
-
+## Runs the GLA removing one constraint each time, to find the maximum success with each subset of constraints
 def minusone(eval_files_dir, iterations, rate, uni):
     eval_files_letters = hg.get_eval_files(eval_files_dir)
     filenames = eval_files_letters[0]
@@ -158,12 +167,15 @@ def minusone(eval_files_dir, iterations, rate, uni):
         max_acc_without[const] = (max_accuracy, max_acc_iter, max_acc_wts, failed_letters, max_acc_s)
 
     logf.write('\n\nSummary\nConstraint\tMax Accuracy\tIterations\tTotal Samples\tFailed Letters\n')
+    print '\n\nSummary\nConstraint\tMax Accuracy\tIterations\tTotal Samples\tFailed Letters\n'
     for const in sorted(max_acc_without.keys()):
-        logf.write('%s\t%.2f\t%d\t%d\t%s\n' % (const, max_acc_without[const][0], max_acc_without[const][1], max_acc_without[const][4],', '.join(failure[0] for failure in max_acc_without[const][3])))
+        text = '%s\t%.2f\t%d\t%d\t%s\n' % (const, max_acc_without[const][0], max_acc_without[const][1], max_acc_without[const][4],', '.join(failure[0] for failure in max_acc_without[const][3]))
+        logf.write(text)
+        print text
 
     logf.close()
 
-
+# Runs the GLA with the same parameters on each of the participant files in a given directory
 def run_all_hg(eval_files_dir, iterations, rate, uni):
     constraints = get_constraints(0)
     grammar = get_constraint_violations(eval_files_dir, constraints)
@@ -173,7 +185,11 @@ def run_all_hg(eval_files_dir, iterations, rate, uni):
     target_files = get_target_files(target_dir)
 
     now = datetime.datetime.now()
-    log_all_filename = 'all_HGlog_%d-%d-%d.txt' % (now.year, now.month, now.day)
+    counter = 1
+    log_all_filename = 'all_HGlog_%d-%d-%d_%d.txt' % (now.year, now.month, now.day, counter)
+    while os.path.isfile(log_all_filename):
+        counter += 1
+        log_all_filename = 'all_HGlog_%d-%d-%d_%d.txt' % (now.year, now.month, now.day, counter)
     log_all = open(log_all_filename, 'w')
 
     all_results = {}
@@ -182,56 +198,98 @@ def run_all_hg(eval_files_dir, iterations, rate, uni):
         data = hg.get_data(target_files[participant])
         print 'Found %d targets...\n' % (len(data))
 
-        # Initialize weights
-        weights = hg.initialize_weights(uni, constraints)
-
         # Open a log file
         log_filename = 'HGlog_%s_%d-%d-%d.txt' % (participant, now.year, now.month, now.day)
         logf = open(log_filename, 'w')
 
-        sum_of_relative_frequencies = sum(int(datum[2]) for datum in data)
+        p_accuracy = 0
+        n = 1
+        while p_accuracy < 1 and n <= 3:
+            logf.write('Participant:\t%sAttempt:\t%d\n' % (participant, n))
 
-        # Evaluate the data and adjust the weights for n iterations
-        print 'Looking for optimal weights for participant %s...' % (participant)
-        final_result = hg.adjust_weights(iterations, data, grammar, weights, rate, sum_of_relative_frequencies, 0)
+            # Initialize weights
+            weights = hg.initialize_weights(uni, constraints)
 
-        max_accuracy = final_result[0]
-        max_acc_iter = final_result[1]
-        max_acc_wts = final_result[2]
-        max_acc_s = final_result[4]
-        total_s = final_result[5]
+            sum_of_relative_frequencies = sum(int(datum[2]) for datum in data)
 
-        # Print a summary of the maximum accuracy and the weights associated with it
-        summary_text = '\nMax accuracy reached: %.2f\nMax accuracy first reached on iteration: %d (%d samples)\nGrammar for max accuracy:\nWeights:\t%s\nConstraints:\t%s' \
-                       % (max_accuracy, max_acc_iter, max_acc_s, '\t'.join(map(str, [round(wt, 2) for wt in max_acc_wts])), '\t'.join(map(str, constraints)))
-        log_all.write('Participant:\t%s%s' % (participant, summary_text))
-        print summary_text
+            # Evaluate the data and adjust the weights for n iterations
+            print 'Looking for optimal weights for participant %s...' % (participant)
+            final_result = hg.adjust_weights(iterations, data, grammar, weights, rate, sum_of_relative_frequencies, 0)
 
-        full_success = hg.write_summary(final_result, constraints, logf)
-        failed_letters = hg.find_failures(grammar, data, constraints, final_result, logf, full_success)  # failed_letters = (letter, target, failures)
-        if full_success == 1:
-            log_all.write('\n\n')
-        else:
-            for failure in failed_letters:
-                text = "\nLetter: %s\tTarget: %s\tCandidates ranked higher than (or tied with) the target:\t%s\n" % (
-                failure[0], failure[1], '\t'.join(failure[2]))
-            log_all.write("%s\n\n" % (text))
+            max_accuracy = final_result[0]
+            max_acc_iter = final_result[1]
+            max_acc_wts = final_result[2]
+            initial_grammar = final_result[3]
+            max_acc_s = final_result[4]
+            total_s = final_result[5]
+            rand_seed = final_result[6]
 
-        all_results[participant] = (max_accuracy, max_acc_iter, total_s, failed_letters)
+            # Print a summary of the maximum accuracy and the weights associated with it
+            summary_text = '\nMax accuracy reached: %.2f\nMax accuracy first reached on iteration: %d (%d samples)\nRandom number generator initialized with seed %d\nInitial grammar weights:\t%s\nGrammar for max accuracy:\t%s\nConstraints:\t%s' \
+                           % (max_accuracy, max_acc_iter, max_acc_s, rand_seed,
+                              '\t'.join(map(str, [round(wt, 2) for wt in initial_grammar])),
+                              '\t'.join(map(str, [round(wt, 2) for wt in max_acc_wts])), '\t'.join(map(str, constraints)))
+            log_all.write('Participant:\t%s\tAttempt:\t%d' % (participant, n))
+            log_all.write('%s' % summary_text)
+            logf.write(summary_text)
+            print summary_text
+
+            failed_letters = hg.find_failures(grammar, data, constraints, final_result, logf, max_accuracy)  # failed_letters = (letter, target, failures)
+            if max_accuracy == 1:
+                log_all.write('\n\n')
+            else:
+                log_all.write('\nLetter\tTarget\tCandidates ranked higher than (or tied with) the target\n')
+                for failure in failed_letters:
+                    text = "%s\t%s\t%s\n" % (failure[0], failure[1], '\t'.join(failure[2]))
+                log_all.write("%s\n\n" % (text))
+
+            if max_accuracy > p_accuracy:
+                p_accuracy = max_accuracy
+                all_results[participant] = (max_accuracy, max_acc_iter, total_s, failed_letters, rand_seed)
+
+            n += 1
 
         logf.close()
 
-    text = "Participant\tMax Accuracy\tIteration for Max\tTotal Samples\n"
+    text = "Participant\tMax Accuracy\tIteration for Max\tTotal Samples\tRandom Seed\tFailed Letters\n"
     print text
     log_all.write(text)
     for p in sorted(all_results.keys()):
-        text = "%s\t%.2f\t%d\t%d" % (p, all_results[p][0], all_results[p][1], all_results[p][2])
+        text = "%s\t%.2f\t%d\t%d\t%d\t%s" % (p, all_results[p][0], all_results[p][1], all_results[p][2], all_results[p][4], ','.join(set([f[0] for f in all_results[p][3]])))
         print text
         log_all.write('%s\n' % (text))
     log_all.close()
 
     return ("Great success")
 
+# Prints a file containing the number of candidates for each letter
+def count_cands(eval_files_dir):
+    constraints_filename = 'AllConst'
+    targets_filename = raw_input('Enter the name of the targets file: ')
+    constraints = hg.get_constraints(constraints_filename)
+    grammar = get_constraint_violations(eval_files_dir, constraints)
+
+    data = hg.get_data(targets_filename)
+
+    now = datetime.datetime.now()
+    counter = 1
+    log_filename = 'Log_CandCounts_%d-%d-%d_%d.txt' % (now.year, now.month, now.day, counter)
+    while os.path.isfile(log_filename):
+        counter += 1
+        log_filename = 'Log_CandCounts_%d-%d-%d_%d.txt' % (now.year, now.month, now.day, counter)
+    logf = open(log_filename, 'w')
+
+    for datum in data:
+        letter = datum[0]
+        n_targets = datum[2]
+        cands = grammar[letter]
+        logf.write('%s\t%d\t%d\n' % (letter, n_targets, len(cands)))
+        print letter, n_targets, len(cands)
+
+
+    return 'success'
+
+# Finds the ID of a target file
 def determine_id(targets_filename):
     if targets_filename[-4:] == '.txt':
         ID = targets_filename[:-4]
@@ -245,6 +303,7 @@ def determine_id(targets_filename):
         ID = ID
     return(ID)
 
+# Gets all the target files from a given directory
 def get_target_files(target_dir):
     os.chdir(target_dir)
     participant_files = {}
@@ -258,6 +317,7 @@ def get_target_files(target_dir):
 
     return (participant_files)
 
+# Gets all the constraints from an active constraints file
 def get_constraints(all):
     # Get constraints and violations
     if all == 1:
@@ -278,12 +338,13 @@ def get_constraints(all):
     print 'Found %d active constraints' % (len(constraints))
     return(constraints)
 
+# Gets all constraint violations from a directory containing eval files
 def get_constraint_violations(eval_files_dir, constraints):
     eval_files_letters = hg.get_eval_files(eval_files_dir)
     filenames = eval_files_letters[0]
     letters = eval_files_letters[1]
     n_letters = len(letters)
-    print 'Found %d active constraints' % (len(constraints))
+    #print 'Found %d active constraints' % (len(constraints))
     print 'Found %d characters in the %s folder' % (n_letters, eval_files_dir)
 
     # Get violations
